2a10c5964c3aa6b881859e2465b16ca9b3b20756
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-07 19:36:17 +0530

    chore: updated one.rs

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -1,5 +1,7 @@
 use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
+use uuid::Uuid;
 
 pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
-  Ok("one".into())
+    tracing::debug!("handler triggered");
+  Ok(Uuid::new_v4().to_string())
 }
