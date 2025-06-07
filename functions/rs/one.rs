09305154645f6502d895c4d9db07cb3916b74a35
lwsrepos <senderemailrs@gmail.com>
2025-06-07 10:27:00 +0530

    Update one.rs

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -1,4 +1,4 @@
-use crate::{prelude::Result, conf::settings, pkg::state::AppState};
+use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
 
 fn handler(state: AppState) -> Result<Vec<u8>>{
   Ok("one".into())
