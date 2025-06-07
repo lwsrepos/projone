48a878b373d59ceb712ffdc7f8fdf578773fa09a
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-07 19:22:06 +0530

    chore: updated one.rs

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -1,5 +1,5 @@
 use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
 
-pub async fn handler(state: AppState) -> Result<Vec<u8>>{
+pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
   Ok("one".into())
 }
