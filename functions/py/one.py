cd9963860164de53417bf0f2c7ebdc3b4d582e45
lwsrepos <senderemailrs@gmail.com>
2025-06-08 18:46:18 +0530

    Update one.py

--- functions/py/one.py
+++ functions/py/one.py
@@ -2,5 +2,5 @@ from pkg.state import AppState
 import uuid
 
 
-def handle(state: AppState, req_id: str|None=None):
+async def handle(state: AppState, req_id: str|None=None):
     return uuid.uuid4().hex.encode()
