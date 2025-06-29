28d30cce154abe6947fdab2f637d03a02b116243
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-29 16:42:41 +0530

    chore: updated image tag :)

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -2,6 +2,7 @@ use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
 use uuid::Uuid;
 
 pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
+    //
     tracing::debug!("handler triggered... ");
     Ok(Uuid::new_v4().to_string().into())
 }
