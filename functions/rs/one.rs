5ca7e97d9ff3539f60b1dc6c580e5f9b85f7fd78
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-29 14:52:08 +0530

    chore: triggering ci

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -3,5 +3,6 @@ use uuid::Uuid;
 
 pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
     tracing::debug!("handler triggered");
-  Ok(Uuid::new_v4().to_string().into())
+    tracing::debug!("post ci - one");
+    Ok(Uuid::new_v4().to_string().into())
 }
