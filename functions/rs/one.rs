0d8dd67819bd28bcedbbd72e5bbd13ee5caf2a07
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-29 15:01:45 +0530

    chore: updated ci

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -3,6 +3,5 @@ use uuid::Uuid;
 
 pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
     tracing::debug!("handler triggered");
-    tracing::debug!("post ci - one");
     Ok(Uuid::new_v4().to_string().into())
 }
