dde0b66dddfc2d85e9f33f97d74d2f087ca2968f
lwsrepos <senderemailrs@gmail.com>
2025-07-02 07:58:29 +0530

    Create joke.rs

---
+++ functions/rs/joke.rs
@@ -0,0 +1,18 @@
+use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
+use uuid::Uuid;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Joke {
+    setup: String,
+    punchline: String,
+}
+
+pub async fn handler(_state: AppState, _req_id: Option<&str>) -> Result<String> {
+    let res = reqwest::get("https://official-joke-api.appspot.com/jokes/random")
+        .await?
+        .json::<Joke>()
+        .await?;
+
+    Ok(format!("{} - {}", res.setup, res.punchline))
+}
