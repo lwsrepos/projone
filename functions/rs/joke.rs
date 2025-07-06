3eaa375bd5d65691f253bed7350fe078a65930dd
lwsrepos <senderemailrs@gmail.com>
2025-07-06 12:59:25 +0530

    Update joke.rs

--- functions/rs/joke.rs
+++ functions/rs/joke.rs
@@ -1,12 +1,11 @@
 use crate::{prelude::Result, pkg::state::AppState};
 use standard_error::StandardError;
-use reqwest;
 
 pub async fn handler(_state: AppState, _req_id: Option<&str>) -> Result<Vec<u8>> {
     let res = reqwest::get("https://official-joke-api.appspot.com/jokes/random")
-        .await.map_err(|e|StandardError::new("ERR-001"))?
+        .await?
         .bytes()
-        .await.map_err(|e| StandardError::new("ERR-002"))?; 
+        .await?; 
     Ok(res.into())
 } 
 
