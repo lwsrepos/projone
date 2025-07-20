b03d978ed9451d0c58770f8d2d6ed9fcd7224418
ashupednekar <ashutoshpednekar15@gmail.com>
2025-07-20 12:40:32 +0530

    feat: support multiple builds

--- functions/rs/one.rs
+++ functions/rs/one.rs
@@ -2,6 +2,6 @@ use crate::{prelude::Result, pkg::conf::settings, pkg::state::AppState};
 use uuid::Uuid;
 
 pub async fn handler(state: AppState, req_id: Option<&str>) -> Result<Vec<u8>>{
-    tracing::debug!("handler triggered... 👍 ");
+    tracing::debug!("one handler triggered... 👍 ");
     Ok(Uuid::new_v4().to_string().into())
 }
