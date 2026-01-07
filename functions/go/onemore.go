84ca819a26766ee0cf1bca036c88970204ca11ad
LiteWebServices Portal <noreply@example.com>
2026-01-07 10:32:12 +0000

    update functions/go/onemore.go

---
+++ functions/go/onemore.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+)
+
+type Input struct {
+    Name *string `json:"name"`
+}
+
+type Output struct {
+    Message string `json:"message"`
+}
+
+func Handle(c *gin.Context) {
+    var input Input
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request"})
+        return
+    }
+
+    name := "stranger"
+    if input.Name != nil && *input.Name != "" {
+        name = *input.Name
+    }
+
+    c.JSON(http.StatusOK, Output{
+        Message: "Hello " + name + " from Go!",
+    })
+}
